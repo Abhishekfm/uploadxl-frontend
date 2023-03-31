1e08fe0cfd331700209b43c69e60f695fdede790
Abhishekfm <62643405+Abhishekfm@users.noreply.github.com>
2023-03-31 15:44:21 +0530

    Multer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 export function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const BACKEND_API = "http://localhost:4000"
-  const [url, setUrl] = useState("")
 
   const handleFileInputChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -14,18 +13,8 @@ export function App() {
   const handleUploadClick = async () => {
     try {
       const formData = new FormData();
-      formData.append("file", selectedFile);
-      formData.append("upload_preset", "carimagecloud");
-      formData.append("cloud_name", process.env.CLOUD_NAME);
-      formData.append("api_key", process.env.CLOUD_API_KEY);
-      const url_res = await axios.post("https://api.cloudinary.com/v1_1/dl7dfvlz8/image/upload", formData)
-      if(!url_res){
-        return
-      }else{
-        console.log(url_res);
-        setUrl(url_res.data.url)
-      }
-      const response = await axios.post(`${BACKEND_API}/api/uploadxl`, url);
+      formData.append('xlFile', selectedFile);
+      const response = await axios.post(`${BACKEND_API}/api/uploadxl`, formData);
       console.log(response.data);
     } catch (error) {
       console.error(error);
